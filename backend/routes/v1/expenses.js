63ff3f25494ed23f3ea107dc88d043a0a6ed7470
Kujanenj <44529387+Kujanenj@users.noreply.github.com>
2020-12-10 18:11:11 +0200

    Simple get/post for expenses

--- backend/routes/v1/expenses.js
+++ backend/routes/v1/expenses.js
@@ -1,15 +1,23 @@
 const express = require('express')
 
+const {addExpense, getAllExpenses, getExpense,updateExpense,deleteExpense} = require("../../database/interface/expenseInterface")
 const router = express.Router()
 
 router.route('/')
 //TODO: get all expenses, send them back
-    .get( ( req, res  ) => {
-        
-        res.json({"test":"test"})
+    .get(async ( req, res  ) => {
+        try{
+            const allExpenses =await  getAllExpenses()
+            res.json(allExpenses)
+        }catch(err){
+            console.log(err)
+            res.status(500).send()
+        }
         //TODO: create a new expense based on req
-    }).post((req,res)=>{
-        res.json({"post":"post"})
+    }).post(async(req,res)=>{
+        console.log(req.body)
+        await addExpense(req.body)
+        res.status(201).json({})
     })
 router.route('')
 module.exports = router
