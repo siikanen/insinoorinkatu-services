0efee8acf92e29f8fcc6ff6615909b08248d2487
Kujanenj <44529387+Kujanenj@users.noreply.github.com>
2020-12-14 17:30:30 +0200

    Fetch settings from database config

--- backend/database/interface/databaseInterface.js
+++ backend/database/interface/databaseInterface.js
@@ -1,33 +1,43 @@
 const { Sequelize, Op, Model, DataTypes } = require("sequelize");
-
+const config = require("../config/config");
 /**
  * Establish a connection to the database
  * Returns a connection object
  */
-async function connectDatabase(){
-    const sequelize = new Sequelize('sqlite::memory:') //TODO: Set custom logger here!
-    try {
-      await sequelize.authenticate();
-      console.log('Connection has been established successfully.');
-    } catch (error) {
-      console.error('Unable to connect to the database:', error);
-    }
-    return sequelize
-
+async function connectDatabase() {
+  const sequelize = new Sequelize(
+    (() => {
+      switch (process.env.NODE_ENV) {
+        case "dev":
+          return config.development;
+        case "staging":
+          return config.test;
+        case "production":
+          return config.production;
+        default:
+          return config.development;
+      }
+    })()
+  ); //TODO: Set custom logger here!
+  try {
+    await sequelize.authenticate();
+    console.log("Connection has been established successfully.");
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
+  return sequelize;
 }
 /**
  * Closes a database connection
  * @param {*} database   - the database connection to close
  */
-async function disconnectDatabase(database){
-    try{
-        console.log("disconnected")
-        database.close()
-    }
-    catch(err){
-        console.log(err)
-    }
+async function disconnectDatabase(database) {
+  try {
+    console.log("disconnected");
+    database.close();
+  } catch (err) {
+    console.log(err);
+  }
 }
 
-
-module.exports  = {connectDatabase,disconnectDatabase}
+module.exports = { connectDatabase, disconnectDatabase };
