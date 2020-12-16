7cfc4cdc7ba30107d3299504fc43ede0923357f4
Kujanenj <44529387+Kujanenj@users.noreply.github.com>
2020-12-16 17:54:32 +0200

    Update random data generation

--- backend/test/API.test.js
+++ backend/test/API.test.js
@@ -12,52 +12,58 @@ const chaiHttp = require("chai-http");
 const faker = require("faker");
 chai.use(chaiHttp);
 
+const randomExpenseAmount = 5;
 // helper function for authorization headers
-const encodeCredentials = (username, password) =>
-  Buffer.from(`${username}:${password}`, 'utf-8').toString('base64');
 
-// helper function for creating randomized test data
-const generateRandomString = (len = 9) => {
-  let str = '';
-
-  do {
-    str += Math.random()
-      .toString(36)
-      .substr(2, 9)
-      .trim();
-  } while (str.length < len);
-
-  return str.substr(0, len);
-};
-
-function getRandomExpenses(amount){
-    let expenses =[]
-    do{
-        expenses.push({
-            "date":generateRandomString(2),
-            "title":generateRandomString(4),
-            "description":generateRandomString(5),
-            "amount":Math.random() * Math.floor(100),
-            "payee":generateRandomString(),
-            "category":generateRandomString()
-
-        })
-    }while(expenses.length < amount)
-    return expenses
+async function generateRandomExpenses(amount) {
+  // TODO: Generate expenses for other users aswell
+  let users = await User.findAll({});
+  let userID = users[0].id;
+  for (i = 0; i < amount; i++) {
+    await Expense.create({
+      title: faker.commerce.productName(),
+      amount: faker.random.number(),
+      userID: userID,
+    });
+  }
 }
-const randomExpenses = getRandomExpenses(5)
-describe('Routes', () => {
+// TODO: password hash and salt not working
+async function generateRandomUsers(amount) {
+  for (i = 0; i < amount; i++) {
+    await User.create({
+      username: `test${i}`,
+      password: "test",
+    });
+  }
+}
+async function generRandomCategories(amount) {
+  for (i = 0; i < amount; i++) {
+    await Category.create({
+      name: faker.commerce.department(),
+    });
+  }
+}
 
-  beforeEach(async () => {
-   await Expense.destroy({
-       where:{},
-       truncate: true
-   })
-   await Expense.bulkCreate(randomExpenses)
-  })
-    describe('Viewing all expenses: GET /api/v1/expenses', () => {
-      it('should respond with "406 Not Acceptable" when Accept header is missing', async () => {
-        const response = await api.get(expenseURL);
+describe("Routes", () => {
+  // Get should not have side effects,
+  // thus setup data only once
+  before(async () => {
+    await Expense.destroy({
+      where: {},
+      truncate: true,
+    });
+    await User.destroy({
+      where: {},
+      truncate: true,
+    });
+    await Category.destroy({
+      where: {},
+      truncate: true,
+    });
+    await generateRandomUsers(1);
+    await generRandomCategories(1);
+    await generateRandomExpenses(randomExpenseAmount);
+  });
         expect(response).to.have.status(406);
       });
 
