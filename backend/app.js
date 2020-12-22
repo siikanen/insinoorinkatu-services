7687407c9d686e461ea62e6b318b93cc7a06b69e
siikanen <santeri.siiranen@pm.me>
2020-12-22 19:15:11 +0200

    Serve docs automatically
    Now docs are served at /api-docs wehever server is running

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const path = require('path');
 const logger = require('morgan');
-const {connectDatabase,disconnectDatabase} = require('./database/interface/databaseInterface') 
+const {connectDatabase,disconnectDatabase} = require('./database/interface/databaseInterface')
 const apiRouterv1 = require('./routes/v1/api');
 const { disconnect } = require('process');
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./swagger.yaml');
+
 
 const app = express();
 
@@ -13,6 +17,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/api/v1', apiRouterv1)
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 //NOTE: testing
 const testDatabase = async()=>{
