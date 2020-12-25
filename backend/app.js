3329e3c37c4fbaf4313a6a7caac94f907f1448b3
siikanen <santeri.siiranen@pm.me>
2020-12-25 19:43:01 +0200

    Move api v1 docs under api v1 path
    Swagger now available under /api/v1/docs

--- backend/app.js
+++ backend/app.js
@@ -1,28 +1,26 @@
-const express = require('express');
-const path = require('path');
-const logger = require('morgan');
-const {connectDatabase,disconnectDatabase} = require('./database/interface/databaseInterface')
-const apiRouterv1 = require('./routes/v1/api');
-const { disconnect } = require('process');
-const swaggerUi = require('swagger-ui-express');
-const YAML = require('yamljs');
-const swaggerDocument = YAML.load('./swagger.yaml');
+const express = require("express");
+const path = require("path");
+const logger = require("morgan");
 
+const apiRouterv1 = require("./routes/v1/api");
+
+const dbInterface = require("./database/interface/databaseInterface");
+
+const sequelize = dbInterface.connect();
+
+const models = require('./database/models')
+
+const test = async () => console.log(await models.Expense.findAll())
+
+test();
 
 const app = express();
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/api/v1', apiRouterv1)
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use("/api/v1", apiRouterv1);
 
-//NOTE: testing
-const testDatabase = async()=>{
-await connectDatabase()
-
-}
-testDatabase()
 module.exports = app;
--- backend/routes/v1/api.js
+++ backend/routes/v1/api.js
@@ -1,8 +1,20 @@
-const express = require('express')
-const expensesRouter = require('./expenses')
-const usersRouter = require('./users')
+const express = require("express");
+const jwt = require("express-jwt");
+const swaggerUi = require("swagger-ui-express");
+const YAML = require("yamljs");
 
-const router = express.Router()
+const expensesRouter = require("./expenses");
+const usersRouter = require("./users");
+
+const swaggerDocument = YAML.load("./routes/v1/swagger.yaml");
+
+const router = express.Router();
+
+router.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+router.route("/").get((req, res) => {
+  res.json({ monni: "test" });
+});
 
 router.route('/')
     .get( ( req, res ) => {
