3438a5999b569a51990fedbca366569197a23542
siikanen <santeri.siiranen@pm.me>
2020-12-25 20:52:15 +0200

    Add login endpoint
    /api/v1/users/login now returns JWT token valid for 1 day given correct username and password via req body

--- backend/routes/v1/users.js
+++ backend/routes/v1/users.js
@@ -1,4 +1,6 @@
+const jwt = require("jsonwebtoken");
 const usersRouter = require("express").Router();
+const models = require("../../database/models");
 
 const User = models.User;
 
@@ -14,9 +16,36 @@ usersRouter
     return res.json(newUser);
   });
 
+usersRouter
+  .route("/login")
 
-router.get('/id', ( req, res ) => {
-    res.send("MONNI")
-})
+  .post(async (req, res) => {
+    const user = await User.findOne({
+      where: {
+        username: req.body.username,
+      },
+    });
+
+    // TODO: Change this to check token in the model without exposing salt 
+    // and passwordhash here
+    const salt = user.salt;
+    const hash = user.passwordHash;
+    if (!user || !user.checkPassword(req.body.password, hash, salt)) {
+      return res.status(401).json({ error: "Invalid username or password" });
+    }
+    console.log("User verification successful");
+
+    const tokenContent = {
+      username: user.username,
+      id: user.id,
+    };
+    const jwtOptions = {
+      algorithm: "HS256",
+      expiresIn: "24h",
+    };
+    const newToken = jwt.sign(tokenContent, process.env.JWT_SECRET, jwtOptions);
+
+    return res.json({ token: newToken });
+  });
 
 module.exports = usersRouter;
