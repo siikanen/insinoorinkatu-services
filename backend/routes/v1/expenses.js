1cdbe7a992a30587c638bd1efe7d6889cf1a8b18
siikanen <santeri.siiranen@pm.me>
2021-01-03 16:57:29 +0200

    Add GET /expenses/:id

--- backend/routes/v1/expenses.js
+++ backend/routes/v1/expenses.js
@@ -13,12 +13,20 @@ expensesRouter
   .get(async (req, res) => {
     // TODO: Validate the request and pass filters from req params to getExpenses()
     const allExpenses = await getExpenses()
-    res.json({data: allExpenses})
+    res.json({ data: allExpenses })
   })
   .post(async (req, res) => {
-    const expensePromises = req.body.data.map(expense => addExpenses(expense))
+    const expensePromises = req.body.data.map((expense) => addExpenses(expense))
     const newExpenses = await Promise.all(expensePromises)
-    res.status(201).json({data:newExpenses})
+    res.status(201).json({ data: newExpenses })
   })
 // TODO: add update and delete methods
+expensesRouter
+  .route('/:id')
+
+  .get(async (req, res) => {
+    return res.json({data: await getExpenses({ id: req.params.id })})
+  })
+
+  })
 module.exports = expensesRouter
