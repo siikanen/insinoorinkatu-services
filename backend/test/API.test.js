419a73aa4a36426ef713d731ba8db0ba2c41459a
Kujanenj <44529387+Kujanenj@users.noreply.github.com>
2021-01-03 18:54:28 +0200

    Update expense tests to use model associations

--- backend/test/API.test.js
+++ backend/test/API.test.js
@@ -6,61 +6,55 @@ const api = supertest(app)
 const expenseURL = '/api/v1/expenses'
 const usersURL = '/api/v1/users'
 const models = require('../database/models')
-const Expense = models.Expense
-const User = models.User
+const { User, Expense } = models
+const {
+  generateRandomExpenses,
+  generateRandomUsers,
+  randomExpenseAmount,
+  randomUserAmount
+} = require('./testHelper')
 const chaiHttp = require('chai-http')
-const faker = require('faker')
 chai.use(chaiHttp)
 
-const randomUserAmount = 2
-const randomExpenseAmount = 5
-let testToken = 'NotYetInitalized'
-
-async function generateRandomExpenses(amount) {
-  // TODO: Generate expenses for other users aswell
-  let users = await User.findAll({})
-  let userID = users[0].id
-  for (let i = 0; i < amount; i++) {
-    await Expense.create({
-      title: faker.commerce.productName(),
-      amount: faker.random.number(),
-      userID: userID,
-    })
-  }
-}
-
-async function generateRandomUsers(amount) {
-  for (let i = 0; i < amount; i++) {
-    await User.create({
-      username: `test${i}`,
-      password: 'test',
-    })
-  }
-}
-
-describe('Expenses', async () => {
-  //Initialize tables and JWT token
-  before(async () => {
-    await User.sync({ force: true })
-    console.log('The table for the User model was just (re)created!')
-    await Expense.sync({ force: true })
-    console.log('The table for the Expense model was just (re)created!')
-    await generateRandomUsers(randomUserAmount)
-    let loginResponse = await api.post(`${usersURL}/login`).send({
-      username: 'test0',
-      password: 'test',
-    })
-    testToken = loginResponse.body.token
+let testToken = 'notYetInitialized'
+async function getToken() {
+  console.log(randomUserAmount)
+  await generateRandomUsers(randomUserAmount)
+  let loginResponse = await api.post(`${usersURL}/login`).send({
+    username: 'test0',
+    password: 'test'
   })
+
+  return loginResponse.body.token
+}
+const testExpenseTemplate = {
+  data: [
+    {
+      title: 'testTitle',
+      description: 'testDescription',
+      amount: 1000,
+      date: new Date(),
+      // Dynamically need to add payee here in order to get ID
+      tags: ['testTag1', 'testTag2']
+    }
+  ]
+}
+
+// Initialize sqlite:memory: before tests
+before(async () => {
+  await models.sequelize.sync({ force: true })
+  testToken = await getToken()
+})
+describe('Expenses', () => {
+  //Initialize tables and JWT token
+  before(async () => {})
   describe('Expenses: GET /api/v1/expenses', () => {
     // Get should not have side effects,
     // thus setup data only once
     before(async () => {
       await Expense.destroy({
-        where: {},
-        truncate: true,
+        where: {}
       })
-
       await generateRandomExpenses(randomExpenseAmount)
     })
     describe('Authorization tests', () => {
@@ -69,7 +63,7 @@ describe('Expenses', async () => {
         expect(response).to.have.status(401)
         expect(response.body.error).to.exist()
       })
-      it('Should respond with 401 unauthorized when JVT token is incorrect', async () => {
+      it('Should respond with 401 unauthorized when JWT token is incorrect', async () => {
         const response = await api
           .get(expenseURL)
           .set('Authorization', 'Bearer notARealToken')
@@ -93,19 +87,45 @@ describe('Expenses', async () => {
           .set('Authorization', `Bearer ${testToken}`)
         expect(response).to.have.status(200)
         expect(response.body.data).to.have.length(randomExpenseAmount)
-      })
-
-      it('Should return filtered expenses when filter ?year=2020 provided', async () => {
-        const response = await api
-          .get(expenseURL + '?year=2020')
-          .set('Authorization', `Bearer ${testToken}`)
-        expect(response).to.have.status(200)
-        let responseExpenses = response.body.data
-        responseExpenses.forEach((value) => {
-          expect(value.date.substring(0, 3)).to.equal('2020')
+        response.body.data.forEach((expense) => {
+          expect(expense).to.have.all.keys(
+            'id',
+            'createdAt',
+            'updatedAt',
+            'title',
+            'description',
+            'amount',
+            'date',
+            'payee',
+            'tags'
+          )
+          expect(expense.payee).to.not.have.any.keys('salt', 'passwordHash')
         })
       })
 
+      /*
+        it('Should return filtered expenses when filter ?year=currentYear provided', async () => {
+          // Add an expense in the past using POST
+          const testDate = new Date()
+          testDate.setFullYear(testDate.getFullYear() - 1)
+          const testExpense2 = { ...testExpenseTemplate.data[0] }
+          testExpense2.date = testDate
+          console.log({ data: [testExpense2] })
+          await api
+            .post(expenseURL)
+            .set('Authorization', `Bearer ${testToken}`)
+            .send({ data: [testExpense2] })
+            console.log("Sent")
+          const response = await api
+            .get(expenseURL + `?year=${currentYear}`)
+            .set('Authorization', `Bearer ${testToken}`)
+          expect(response).to.have.status(200)
+          let responseExpenses = response.body.data
+          responseExpenses.forEach((value) => {
+            expect(new Date(value.date).getFullYear()).to.equal(currentYear)
+          })
+        })
+  */
       it('Should return empty data array when no expenses found', async () => {
         //There "shouldn't" be any expenses for the year 1
         const response = await api
@@ -124,55 +144,50 @@ describe('Expenses', async () => {
       })
     })
   })
-  describe('Expenses: POST /api/expenses', () => {
+  describe('Expenses: POST /api/v1/expenses', () => {
     //Initiate a test expense
-    let testExpense
 
     before(async () => {
-      await Expense.destroy({
-        where: {},
-        truncate: true,
-      })
-
-      //Set test expense fields
-      let users = await User.findAll({})
-      let userID = users[0].id
-      testExpense = {
-        title: 'test Title',
-        description: 'test description',
-        amount: 1000,
-        //TODO: model currenly does not support date
-        date: {},
-        //TODO: revise if payee ID is enough
-        payee: {
-          id: userID,
-          username: users[0].username,
-        },
-        tags: ['testTag', 'secondTestTag'],
+      await models.sequelize.sync({ force: true })
+      // await Expense.destroy({
+      //   where: {},
+      //   truncate: true,
+      //   cascade: true
+      // })
+      // await User.destroy({
+      //   where: {},
+      //   truncate: true,
+      //   cascade: true
+      // })
+      await generateRandomUsers(randomUserAmount)
+      let testUser = await User.findOne({})
+      testExpenseTemplate.data[0].payee = {
+        id: testUser.id,
+        username: testUser.username
       }
     })
+
     beforeEach(async () => {
       // No need to clear anything else than expenses between tests
       await Expense.destroy({
-        where: {},
-        truncate: true,
+        where: {}
       })
     })
     describe('Authorization tests', () => {
       it('Should respond with 401 unauthorized when Authorization header is missing', async () => {
-        const response = await api.post(expenseURL).send(testExpense)
+        const response = await api.post(expenseURL).send(testExpenseTemplate)
         expect(response).to.have.status(401)
-        expect(response.body).to.equal(undefined)
+        expect(response.body.data).to.equal(undefined)
         const expensesInDB = await Expense.findAll({})
         expect(expensesInDB).to.have.length(0)
       })
-      it('Should respond with 401 unauthorized when JVT token is incorrect', async () => {
+      it('Should respond with 401 unauthorized when JWT token is incorrect', async () => {
         const response = await api
           .post(expenseURL)
           .set('Authorization', 'Bearer notARealToken')
-          .send(testExpense)
+          .send(testExpenseTemplate)
         expect(response).to.have.status(401)
-        expect(response.body).to.equal(undefined)
+        expect(response.body.data).to.equal(undefined)
         const expensesInDB = await Expense.findAll({})
         expect(expensesInDB).to.have.length(0)
       })
@@ -183,7 +198,7 @@ describe('Expenses', async () => {
         .post(expenseURL)
         .set('Authorization', `Bearer ${testToken}`)
         .send({
-          notRealField: 2,
+          notRealField: 2
         })
       expect(response).to.have.status(400)
       expect(response.body.error).to.exist()
@@ -194,60 +209,70 @@ describe('Expenses', async () => {
       const response = await api
         .post(expenseURL)
         .set('Authorization', `Bearer ${testToken}`)
-        .send(testExpense)
+        .send(testExpenseTemplate)
       const expensesInDB = await Expense.findAll({})
       expect(response).to.have.status(201)
       expect(expensesInDB).to.have.length(1)
-      expect(expensesInDB[0].title).to.be.equal('testTitle')
+      expect(expensesInDB[0].title).to.be.equal(
+        testExpenseTemplate.data[0].title
+      )
     })
     it('Should return the added expense', async () => {
       const response = await api
         .post(expenseURL)
         .set('Authorization', `Bearer ${testToken}`)
-        .send(testExpense)
-      const expensesInDB = await Expense.findAll({})
+        .send(testExpenseTemplate)
       expect(response).to.have.status(201)
-      expect(response.body.data).to.have.all.keys(
-        'id',
-        'created',
-        'updated',
-        'title',
-        'description',
-        'amount',
-        'date',
-        'payee',
-        'tags'
-      )
+      expect(response.body.data).to.be.an('Array')
+      response.body.data.forEach((expense) => {
+        expect(expense).to.have.all.keys(
+          'id',
+          'createdAt',
+          'updatedAt',
+          'title',
+          'description',
+          'amount',
+          'date',
+          'payee',
+          'tags'
+        )
+        expect(expense.payee).to.not.have.any.keys('salt', 'passwordHash')
+      })
     })
 
     it('Should add multiple new expenses, when multiple expenses are provided', async () => {
-      const testExpense2 = { ...testExpense }
+      const testExpense2 = { ...testExpenseTemplate.data[0] }
       testExpense2.title = 'TitleIsChanged'
+      const copyTemplate = { ...testExpenseTemplate }
+      copyTemplate.data.push(testExpense2)
       const response = await api
         .post(expenseURL)
         .set('Authorization', `Bearer ${testToken}`)
         //TODO: Might need to send in a different format
-        .send([testExpense, testExpense2])
+        .send(copyTemplate)
       const expensesInDB = await Expense.findAll({})
       expect(response).to.have.status(201)
       expect(expensesInDB).to.have.length(2)
       expect(response.body.data).to.have.length(2)
+      expect(response.body.data[0].title).to.equal(
+        testExpenseTemplate.data[0].title
+      )
+      expect(response.body.data[1].title).to.equal(
+        testExpenseTemplate.data[1].title
+      )
     })
   })
-  describe('Expenses: GET /api/expenses/{id}', () => {
+  describe('Expenses: GET /api/v1/expenses/{id}', () => {
     let testExpense
     let testExpenseID
     let singleExpenseURL
     before(async () => {
       await Expense.destroy({
-        where: {},
-        truncate: true,
+        where: {}
       })
 
       await generateRandomExpenses(randomExpenseAmount)
-      //Some overhead here, assining array[0] directly from findall does not seem to work
-      testExpense = await Expense.findAll({})
-      testExpense = testExpense[0]
+      testExpense = await Expense.findOne()
       testExpenseID = testExpense.id
       singleExpenseURL = expenseURL + `/${testExpenseID}`
     })
@@ -257,7 +282,7 @@ describe('Expenses', async () => {
         expect(response).to.have.status(401)
         expect(response.body.error).to.exist()
       })
-      it('Should respond with 401 unauthorized when JVT token is incorrect', async () => {
+      it('Should respond with 401 unauthorized when JWT token is incorrect', async () => {
         const response = await api
           .get(singleExpenseURL)
           .set('Authorization', 'Bearer notARealToken')
@@ -286,8 +311,8 @@ describe('Expenses', async () => {
       expect(response.body.data.id).to.equal(testExpenseID)
       expect(response.body.data).to.have.all.keys(
         'id',
-        'created',
-        'updated',
+        'createdAt',
+        'updatedAt',
         'title',
         'description',
         'amount',
@@ -295,37 +320,40 @@ describe('Expenses', async () => {
         'payee',
         'tags'
       )
+      expect(response.body.data.payee).to.not.have.any.keys(
+        'salt',
+        'passwordHash'
+      )
     })
   })
   describe('Expenses: PUT /api/expenses/{id}', () => {
     let testExpense
-    let testExpenseID
     let modifiedTestExpense
+
     let singleExpenseURL
     before(async () => {
-      await Expense.destroy({
-        where: {},
-        truncate: true,
-      })
+      await models.sequelize.sync({ force: true })
+      await generateRandomUsers(randomUserAmount)
+      let userFromDb = await User.findOne()
+      modifiedTestExpense = { ...testExpenseTemplate.data[0] }
+      modifiedTestExpense.payee = {
+        username: userFromDb.username,
+        id: userFromDb.id
+      }
+      modifiedTestExpense.title = 'Modified'
+      modifiedTestExpense.amount = 1337
     })
     beforeEach(async () => {
       // No need to clear anything else than expenses between tests
       await Expense.destroy({
-        where: {},
-        truncate: true,
+        where: {}
       })
       // TODO: Change the amount of expenses generated
-      // In theory, it is enough to test with a singe expense when deleting
+      // In theory, it is enough to test with a sinlge expense when deleting
       // Clearing the expenses causes the ID to change, so it needs te be re-aquired
       await generateRandomExpenses(randomExpenseAmount)
-      //Some overhead here, assining array[0] directly from findall does not seem to work
-      testExpense = await Expense.findAll({})
-      testExpense = testExpense[0]
-      modifiedTestExpense = { ...testExpense }
-      modifiedTestExpense.title = 'Modified'
-      modifiedTestExpense.amount = 1337
-      testExpenseID = testExpense.id
-      singleExpenseURL = expenseURL + `/${testExpenseID}`
+      testExpense = await Expense.findOne({})
+      singleExpenseURL = expenseURL + `/${testExpense.id}`
     })
     describe('Authorization tests', () => {
       it('Should respond with 401 unauthorized when Authorization header is missing', async () => {
@@ -333,7 +361,7 @@ describe('Expenses', async () => {
         expect(response).to.have.status(401)
         expect(response.body.error).to.exist()
       })
-      it('Should respond with 401 unauthorized when JVT token is incorrect', async () => {
+      it('Should respond with 401 unauthorized when JWT token is incorrect', async () => {
         const response = await api
           .put(singleExpenseURL)
           .set('Authorization', 'Bearer notARealToken')
@@ -351,57 +379,42 @@ describe('Expenses', async () => {
     })
     it('Should respond with 200', async () => {
       const response = await api
-        .get(singleExpenseURL)
+        .put(singleExpenseURL)
         .set('Authorization', `Bearer ${testToken}`)
         .send(modifiedTestExpense)
       expect(response).to.have.status(200)
     })
     it('Should modify the expense', async () => {
       const response = await api
-        .get(singleExpenseURL)
+        .put(singleExpenseURL)
         .set('Authorization', `Bearer ${testToken}`)
         .send(modifiedTestExpense)
       expect(response).to.have.status(200)
-      let expenseInDB = await Expense.findOne({ where: { id: testExpenseID } })
-      expect(expenseInDB).to.have.all.keys(
-        'id',
-        'created',
-        'updated',
-        'title',
-        'description',
-        'amount',
-        'date',
-        'payee',
-        'tags'
-      )
+      let expenseInDB = await Expense.findOne({
+        where: { id: testExpense.id }
+      })
       expect(expenseInDB.title).to.equal('Modified')
       expect(expenseInDB.amount).to.equal(1337)
     })
   })
   describe('Expenses: DELETE /api/expenses/{id}', () => {
     let testExpense
-    let testExpenseID
     let singleExpenseURL
     before(async () => {
       await Expense.destroy({
-        where: {},
-        truncate: true,
+        where: {}
       })
     })
     beforeEach(async () => {
       // No need to clear anything else than expenses between tests
       await Expense.destroy({
-        where: {},
-        truncate: true,
+        where: {}
       })
       // TODO: Change the amount of expenses generated
       // In theory, it is enough to test with a singe expense when deleting
       await generateRandomExpenses(randomExpenseAmount)
-      //Some overhead here, assining array[0] directly from findall does not seem to work
-      testExpense = await Expense.findAll({})
-      testExpense = testExpense[0]
-      testExpenseID = testExpense.id
-      singleExpenseURL = expenseURL + `/${testExpenseID}`
+      testExpense = await Expense.findOne({})
+      singleExpenseURL = expenseURL + `/${testExpense.id}`
     })
 
     describe('Authorization tests', () => {
@@ -410,7 +423,7 @@ describe('Expenses', async () => {
         expect(response).to.have.status(401)
         expect(response.body.error).to.exist()
       })
-      it('Should respond with 401 unauthorized when JVT token is incorrect', async () => {
+      it('Should respond with 401 unauthorized when JWT token is incorrect', async () => {
         const response = await api
           .delete(singleExpenseURL)
           .set('Authorization', 'Bearer notARealToken')
