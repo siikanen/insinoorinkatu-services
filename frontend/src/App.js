4b89bc179e4676fd6b623f13d38db7dd5b463137
Kujanenj <44529387+Kujanenj@users.noreply.github.com>
2021-01-04 22:15:31 +0200

    Initial setup for FRONT END
    Setup enviroment, dependencies and base react
    app for front end developement

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import Container from '@material-ui/core/Container'
+import LoginForm from './components/login/LoginForm'
+
+function App() {
+  return (
+    <Container>
+      <div className="App">
+      </div>
+    </Container>
+  )
+}
+
+export default App
