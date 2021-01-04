9da2901a4508d61d2542ca7fd160d430de51e859
Kujanenj <44529387+Kujanenj@users.noreply.github.com>
2021-01-04 22:07:03 +0200

    Add cors dependency

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 require('express-async-errors')
+const cors = require('cors')
 const path = require('path')
 const logger = require('morgan')
 
@@ -7,7 +8,7 @@ const errorHandler = require('./utils/errors/handler')
 const apiRouterv1 = require('./routes/v1/api')
 
 const app = express()
-
+app.use(cors())
 app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
