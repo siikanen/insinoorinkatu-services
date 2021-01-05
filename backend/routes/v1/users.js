89b0a4d24ea5908aa65dfe66dd33c09723992d58
siikanen <santeri.siiranen@pm.me>
2021-01-05 14:10:44 +0200

    Add all CRUD methods for users

--- backend/routes/v1/users.js
+++ backend/routes/v1/users.js
@@ -1,9 +1,10 @@
 const jwt = require('jsonwebtoken')
 const usersRouter = require('express').Router()
-const models = require('../../database/models')
-const { UserValidationError } = require('../../utils/errors/userfacing')
-
-const User = models.User
+const User = require('../../database/models').User
+const {
+  UserValidationError,
+  NotFoundError
+} = require('../../utils/errors/userfacing')
 
 usersRouter
   .route('/')
@@ -16,7 +17,7 @@ usersRouter
   .post(async (req, res) => {
     const newUser = await User.create({
       username: req.body.username,
-      password: req.body.password,
+      password: req.body.password
     })
     return res.json(newUser)
   })
@@ -27,13 +28,13 @@ usersRouter
   .post(async (req, res) => {
     const user = await User.findOne({
       where: {
-        username: req.body.username,
-      },
+        username: req.body.username
+      }
     })
 
     if (
       !user ||
-      typeof req.body.password !== "string" ||
+      typeof req.body.password !== 'string' ||
       !user.checkPassword(req.body.password)
     )
       throw new UserValidationError('Invalid username or password')
@@ -43,15 +44,46 @@ usersRouter
 
     const tokenContent = {
       username: user.username,
-      id: user.id,
+      id: user.id
     }
     const jwtOptions = {
       algorithm: 'HS256',
-      expiresIn: '24h',
+      expiresIn: '24h'
     }
     const newToken = jwt.sign(tokenContent, process.env.JWT_SECRET, jwtOptions)
 
     return res.json({ token: newToken })
+  })
+
+usersRouter
+  .route('/:id')
+
+  // Find user or throw before doing anything else
+  .all(async (req, res, next) => {
+    const user = await User.findByPk(req.params.id)
+    if (!user) throw new NotFoundError('No user has given id')
+    req.user = user
+    next()
+  })
+
+  .get(async (req, res) => {
+    return res.json({ data: req.user })
+  })
+
+  .put(async (req, res) => {
+    user = req.user
+    const updatedUser = await user.update(req.body, {
+      fields: ['username', 'password']
+    })
+    // We have to call save because password field is virtual
+    // and will not get saved to the db by sequelize without
+    await user.save()
+    return res.json({ data: updatedUser })
+  })
+
+  .delete(async (req, res) => {
+    await req.user.destroy()
+    return res.status(204).end()
   })
 
 module.exports = usersRouter
