9e2e0e8a3b736c68b276710539ec96c392582e46
Kujanenj <44529387+Kujanenj@users.noreply.github.com>
2021-01-07 15:52:39 +0200

    Add update expense view, and a route for it

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -4,7 +4,10 @@ import MainLayout from './layouts/MainLayout'
 import Expense from './view/Expenses/ExpenseView/Expense'
 import DashboardView from './view/dashboard/dashboardView'
 import LoginForm from './components/login/LoginForm'
+import UpdateExpenseView from './view/Expenses/UpdateExpenseView/UpdateExpenseView'
 import CreateExpenseView from './view/Expenses/CreateExpenseView/CreateExpenseView'
+import NotFoundView from './view/errors/NotFoundView'
+import { Navigate } from 'react-router'
 
 const routes = [
   {
@@ -13,10 +16,16 @@ const routes = [
     children: [
       { path: 'dashboard', element: <DashboardView /> },
       {
+        path: 'expenses/update/:id',
+        element: <UpdateExpenseView></UpdateExpenseView>
+      },
+      {
         path: 'expenses/create',
         element: <CreateExpenseView></CreateExpenseView>
       },
-      { path: 'expenses/:id', element: <Expense></Expense> }
+      { path: 'expenses/:id', element: <Expense></Expense> },
+      { path: '404', element: <NotFoundView /> },
+      { path: '*', element: <Navigate to="/app/404"></Navigate> }
     ]
   },
   {
---
+++ frontend/src/view/Expenses/UpdateExpenseView/UpdateExpenseView.js
@@ -0,0 +1,135 @@
+import React, { useState, useEffect } from 'react'
+import { useParams, useNavigate } from 'react-router-dom'
+import expensesService from '../../../services/expenses'
+import { useDispatch } from 'react-redux'
+import {
+  Box,
+  Button,
+  Container,
+  TextField,
+  Typography,
+  makeStyles
+} from '@material-ui/core'
+import Page from '../../../components/Page'
+import { updateExpense } from '../../../reducers/expensesReducer'
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    backgroundColor: theme.palette.background.dark,
+    height: '100%',
+    paddingBottom: theme.spacing(15),
+    paddingTop: theme.spacing(0)
+  }
+}))
+
+const UpdateExpenseView = () => {
+  const [expense, setExpense] = useState()
+  let navigate = useNavigate()
+  const classes = useStyles()
+  const { id } = useParams()
+  const dispatch = useDispatch()
+  const loggedInUser = JSON.parse(window.localStorage.getItem('loggedUser'))
+  // let loggedInUser = useSelector(({ users }) => {
+  //   return users.loggedInUser
+  // })
+  useEffect(() => {
+    expensesService
+      .getExpenseById(loggedInUser.token, id)
+      .then((value) => {
+        setExpense(value.data[0])
+      })
+      .catch((err) => navigate('/app/404'))
+  }, [])
+  if (!expense) {
+    return <div></div>
+  }
+  const handleSubmit = async (event) => {
+    event.preventDefault()
+    const title = event.target.title.value
+    const amount = event.target.amount.value
+    const description = event.target.description.value
+    const tags = event.target.tags.value
+    dispatch(
+      updateExpense(loggedInUser.token, {
+        id,
+        title,
+        description,
+        amount,
+        date: {},
+        payee: {
+          id: loggedInUser.id,
+          username: loggedInUser.username
+        },
+        tags: [tags]
+      })
+    )
+  }
+  return (
+    <Page className={classes.root} title="UpdateExpense">
+      <Box
+        display="flex"
+        flexDirection="column"
+        height="100%"
+        justifyContent="center"
+      >
+        <Container maxWidth="sm">
+          <form onSubmit={handleSubmit}>
+            <Box mb={1}>
+              <Typography color="textPrimary" variant="h2">
+                Update an expense
+              </Typography>
+              <Typography>{expense.id}</Typography>
+            </Box>
+            <TextField
+              defaultValue={expense.title}
+              fullWidth
+              label="Title"
+              margin="normal"
+              name="title"
+              variant="outlined"
+            />
+            <TextField
+              defaultValue={expense.description}
+              fullWidth
+              label="Description"
+              margin="normal"
+              name="description"
+              variant="outlined"
+            />
+            <TextField
+              fullWidth
+              defaultValue={expense.amount}
+              label="Amount"
+              type="number"
+              margin="normal"
+              name="amount"
+              variant="outlined"
+            />
+            <TextField
+              defaultValue={expense.tags}
+              fullWidth
+              label="Tags"
+              margin="normal"
+              name="tags"
+              variant="outlined"
+            />
+
+            <Box my={3}>
+              <Button
+                color="primary"
+                fullWidth
+                size="large"
+                type="submit"
+                variant="contained"
+              >
+                Update Expense
+              </Button>
+            </Box>
+          </form>
+        </Container>
+      </Box>
+    </Page>
+  )
+}
+
+export default UpdateExpenseView
