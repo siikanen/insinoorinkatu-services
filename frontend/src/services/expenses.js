f160124f6aca99cc8f89b2b10c18060f559c0860
Kujanenj <44529387+Kujanenj@users.noreply.github.com>
2021-01-07 15:53:10 +0200

    Add update expense service

--- frontend/src/services/expenses.js
+++ frontend/src/services/expenses.js
@@ -1,32 +1,47 @@
 import axios from 'axios'
 const baseUrl = 'http://localhost:3000/api/v1/expenses'
-
+// TODO Make a better exception handling
 const getAllExpenses = async (token) => {
-
-   const config = { headers: { Authorization: `Bearer ${token}` }, }
-   try{
-     const response = await axios.get(baseUrl, config)
-     return response.data
-   }
-   catch(err){
-     console.error(err)
-     return []
-   }
+  const config = { headers: { Authorization: `Bearer ${token}` } }
+  try {
+    const response = await axios.get(baseUrl, config)
+    return response.data
+  } catch (err) {
+    console.error(err)
+    return []
+  }
 }
-const createExpense = async(token,expenseToAdd)=>{
-
-  console.log(expenseToAdd)
-   const config = { headers: { Authorization: `Bearer ${token}` }, }
-   let data = {data:[expenseToAdd
-  ]}
-  console.log(data)
-   try{
-     const response = await axios.post(baseUrl,data,config)
-     return response.data
-   }
-   catch(err){
-     console.error(err)
-     return undefined
-   }
+const getExpenseById = async (token, id) => {
+  const config = { headers: { Authorization: `Bearer ${token}` } }
+  try {
+    const response = await axios.get(`${baseUrl}/${id}`, config)
+    return response.data
+  } catch (err) {
+    console.error(err)
+    return {}
+  }
 }
-export default { getAllExpenses,createExpense }
+const updateExpense = async (token,id, expenseToUpdate) => {
+  const config = { headers: { Authorization: `Bearer ${token}` } }
+  let data = { data: expenseToUpdate }
+
+  try {
+    await axios.put(`${baseUrl}/${id}`, data,config)
+    return
+  } catch (err) {
+    console.error(err)
+    return undefined
+  }
+}
+const createExpense = async (token, expenseToAdd) => {
+  const config = { headers: { Authorization: `Bearer ${token}` } }
+  let data = { data: [expenseToAdd] }
+  try {
+    const response = await axios.post(baseUrl, data, config)
+    return response.data
+  } catch (err) {
+    console.error(err)
+    return undefined
+  }
+}
+export default { getAllExpenses, createExpense,getExpenseById,updateExpense }
