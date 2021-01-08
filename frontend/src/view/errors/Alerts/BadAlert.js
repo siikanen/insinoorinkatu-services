8198b9b15fc0ebd252b3210689d6162ec61375d1
Kujanenj <44529387+Kujanenj@users.noreply.github.com>
2021-01-08 17:28:55 +0200

    Add alert components

---
+++ frontend/src/view/errors/Alerts/BadAlert.js
@@ -0,0 +1,11 @@
+import ErrorOutlineIcon from '@material-ui/icons/ErrorOutline';
+import React from 'react';
+import Alert from '@material-ui/lab/Alert';
+const BadAlert = (props) => {
+  return (
+   <Alert icon={<ErrorOutlineIcon fontSize="inherit" />} severity="error">
+     {props.message}
+      </Alert>
+  )
+}
+export default BadAlert
---
+++ frontend/src/view/errors/Alerts/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Collapse from '@material-ui/core/Collapse';
+import BadAlert from './BadAlert'
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: '100%',
+    '& > * + *': {
+      marginTop: theme.spacing(2)
+    }
+  }
+}))
+
+const GenericAlert = (props) => {
+
+  const classes = useStyles()
+  if(props.alertOpen){
+    switch (props.type) {
+      case 'Error':
+        return (
+          <div className={classes.root}>
+          <Collapse in={props.alertOpen}>
+            <BadAlert message={props.message}></BadAlert>
+          </Collapse>
+        </div>
+      )
+      default:
+        return <div></div>
+      }
+    }
+    return <div></div>
+}
+export default GenericAlert
