801a7f53ec15968b7f21c5372735c83227b5803b
siikanen <santeri.siiranen@pm.me>
2021-01-08 19:04:51 +0200

    Update Expenses table to match new fields

--- backend/database/migrations/2-create-expenses.js
+++ backend/database/migrations/2-create-expenses.js
@@ -5,40 +5,47 @@ module.exports = {
       id: {
         allowNull: false,
         primaryKey: true,
-        type: Sequelize.DataTypes.UUID,
+        type: Sequelize.DataTypes.UUID
       },
       title: {
         type: Sequelize.STRING,
+        allowNull: false
       },
       description: {
-        type: Sequelize.STRING,
+        type: Sequelize.STRING(1500),
+        allowNull: true
       },
-      amount: {
+      price: {
         type: Sequelize.INTEGER,
+        allowNull: false
       },
       date: {
         type: Sequelize.DATE,
+        allowNull: false
+      },
+      resolved: {
+        type: Sequelize.BOOLEAN
       },
       UserId: {
         type: Sequelize.DataTypes.UUID,
         references: {
           model: 'Users',
-          key: 'id',
+          key: 'id'
         },
         onUpdate: 'CASCADE',
-        onDelete: 'SET NULL',
+        onDelete: 'SET NULL'
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE,
+        type: Sequelize.DATE
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE,
-      },
+        type: Sequelize.DATE
+      }
     })
   },
   down: async (queryInterface, Sequelize) => {
     await queryInterface.dropTable('Expenses')
-  },
+  }
 }
