a2c1dc684ac50c5ed79035f8b7919843ce5815bd
Kujanenj <44529387+Kujanenj@users.noreply.github.com>
2021-01-12 21:07:57 +0200

    Update price formatting for expense service

--- frontend/src/services/expenses.js
+++ frontend/src/services/expenses.js
@@ -1,42 +1,52 @@
 import axios from 'axios'
 const baseUrl = `${process.env.REACT_APP_SERVER_URL}/expenses`
 //Format the price, e.g 1010 = 10,10
-const formatPrice = function (expense) {
-  expense.price = expense.price/100
-  return expense
+const intToPrice = (price) => {
+  return price / 100
 }
-const getToken = function(){
+const priceToInt = (price) => {
+  return Math.trunc(price * 100)
+}
+const getToken = function () {
   return JSON.parse(window.localStorage.getItem('loggedUser')).token
 }
 const getAllExpenses = async () => {
   const config = { headers: { Authorization: `Bearer ${getToken()}` } }
   const response = await axios.get(baseUrl, config)
-  return response.data.data.map(formatPrice)
+  return response.data.data.map((expense) => {
+    return { ...expense, price: intToPrice(expense.price) }
+  })
 }
-const getExpenseById = async ( id) => {
+const getExpenseById = async (id) => {
   const config = { headers: { Authorization: `Bearer ${getToken()}` } }
   const response = await axios.get(`${baseUrl}/${id}`, config)
   //TODO: Remove [0] when backend is fixed
-  return formatPrice(response.data.data[0])
+  return { ...response.data.data[0], price: intToPrice(response.data.data[0].price) }
 }
-const updateExpense = async ( id, expenseToUpdate) => {
+const updateExpense = async (id, expenseToUpdate) => {
   const config = { headers: { Authorization: `Bearer ${getToken()}` } }
   const data = { data: expenseToUpdate }
 
   await axios.put(`${baseUrl}/${id}`, data, config)
 }
-const deleteExpense = async ( id) => {
+const deleteExpense = async (id) => {
   const config = { headers: { Authorization: `Bearer ${getToken()}` } }
 
   await axios.delete(`${baseUrl}/${id}`, config)
 }
-const createExpense = async ( expenseToAdd) => {
+const createExpense = async (expenseToAdd) => {
   const config = { headers: { Authorization: `Bearer ${getToken()}` } }
-  const data = { data: [expenseToAdd] }
+  const expenseToSend = {
+    ...expenseToAdd,
+    price: priceToInt(expenseToAdd.price)
+  }
+  const data = {
+    data: [expenseToSend]
+  }
   const response = await axios.post(baseUrl, data, config)
-  return response.data.data.map(formatPrice)
+  return response.data.data.map(intToPrice)
 }
-const expenseService= {
+const expenseService = {
   getAllExpenses,
   createExpense,
   getExpenseById,
