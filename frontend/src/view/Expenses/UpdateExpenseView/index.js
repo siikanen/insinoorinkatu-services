5e7f4de96581b896ebb7368f2ca43f0520fad437
Kujanenj <44529387+Kujanenj@users.noreply.github.com>
2021-01-14 17:26:52 +0200

    Add index file for UpdateExpense

---
+++ frontend/src/view/Expenses/UpdateExpenseView/index.js
@@ -0,0 +1,79 @@
+import React, { useEffect, useState } from 'react'
+import { useDispatch } from 'react-redux'
+import {
+  Container,
+  Grid,
+  makeStyles,
+} from '@material-ui/core'
+import Page from '../../../components/Page'
+import { useNavigate, useParams } from 'react-router-dom'
+import expensesService from '../../../services/expenses'
+import DeleteDialog from '../ExpenseView/DeleteExpense'
+import { deleteExpense } from '../../../reducers/expensesReducer'
+import UpdateExpense from './UpdateExpense'
+const useStyles = makeStyles((theme) => ({
+  root: {
+    backgroundColor: theme.palette.background.dark,
+    minHeight: '100%',
+    paddingBottom: theme.spacing(3),
+    paddingTop: theme.spacing(3)
+  }
+}))
+
+const UpdateExpenseView = () => {
+  const classes = useStyles()
+  const [expense, setExpense] = useState()
+  const [dialogOpen, setDialogOpen] = useState(false)
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+  const { id } = useParams()
+  useEffect(() => {
+    expensesService
+      .getExpenseById(id)
+      .then((value) => {
+        setExpense(value)
+      })
+      .catch((err) => {
+        console.error(err)
+        // navigate('/app/404')
+      })
+  }, [id, navigate])
+  if (!expense) {
+    return <div></div>
+  }
+  const handleDeleteClick = () => {
+    setDialogOpen(true)
+  }
+  const handleClose = () => {
+    setDialogOpen(false)
+  }
+  const handleConfirmDelete = () => {
+    dispatch(deleteExpense(id))
+    setDialogOpen(false)
+    setExpense(null)
+    navigate('/app/expenses')
+  }
+  console.log(expense)
+  return (
+    <Page className={classes.root}>
+      <Container maxWidth={false}>
+        <Grid container spacing={1}>
+          <DeleteDialog
+            dialogOpen={dialogOpen}
+            handleClose={handleClose}
+            handleConfirmDelete={handleConfirmDelete}
+          ></DeleteDialog>
+
+          <Grid item sm={4}>
+            <UpdateExpense
+              expense={expense}
+              handleDeleteClick={handleDeleteClick}
+            ></UpdateExpense>
+          </Grid>
+        </Grid>
+      </Container>
+    </Page>
+  )
+}
+
+export default UpdateExpenseView
