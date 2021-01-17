aa5dd8bed3e88c62f3be8159f1b1286b6a8c82b9
Kujanenj <44529387+Kujanenj@users.noreply.github.com>
2021-01-17 17:32:26 +0200

    Add services for registering and getting all users. Add matching actions for user store

--- frontend/src/reducers/usersReducer.js
+++ frontend/src/reducers/usersReducer.js
@@ -2,16 +2,46 @@ import usersService from '../services/users'
 export const logIn = (user) => {
   return async (dispatch) => {
     const loggedInUser = await usersService.logIn(user)
-    window.localStorage.setItem('loggedUser',JSON.stringify(loggedInUser))
+    window.localStorage.setItem('loggedUser', JSON.stringify(loggedInUser))
 
     dispatch({
       type: 'SET_LOGGED_USER',
-      data: {user}
+      data: { user }
     })
   }
 }
-const usersReducers = (state = {}, action) => {
+export const addUser = (userToAdd) => {
+  return async (dispatch) => {
+    const response = await usersService.register(userToAdd)
+    const data = response.data
+    dispatch({
+      type: 'NEW_USER',
+      data
+    })
+  }
+}
+export const getUsers = (serchParams = {}) => {
+  return async (dispatch) => {
+    const response = await usersService.getUsers()
+    const data = response.data.data
+    dispatch({
+      type: 'SET_USERS',
+      data
+    })
+  }
+}
+const usersReducers = (
+  state = {
+    allUsers: [],
+    loggedInUser: {}
+  },
+  action
+) => {
   switch (action.type) {
+  case 'SET_USERS':
+    return { ...state, allUsers: action.data }
+  case 'NEW_USER':
+    return { ...state, allUsers: state.allUsers.concat(action.data) }
   case 'SET_LOGGED_USER':
     return { ...state, loggedInUser: action.data }
   default:
--- frontend/src/services/users.js
+++ frontend/src/services/users.js
@@ -1,15 +1,23 @@
 import axios from 'axios'
 import jwt_decode from 'jwt-decode'
+import { getToken } from '../utils/utils'
 const baseUrl = `${process.env.REACT_APP_SERVER_URL}/users`
 
 const logIn = async (credentials) => {
-  const response= await axios.post(baseUrl + '/login', credentials)
-  const {username,id} = jwt_decode(response.data.token)
+  const response = await axios.post(baseUrl + '/login', credentials)
+  const { username, id } = jwt_decode(response.data.token)
   return {
     id,
     username,
-    token:response.data.token
+    token: response.data.token
   }
 }
-const userService = {logIn}
+const register = async (userToAdd) => {
+  return await axios.post(baseUrl, userToAdd)
+}
+const getUsers = async () => {
+  const config = { headers: { Authorization: `Bearer ${getToken()}` } }
+  return await axios.get(baseUrl, config)
+}
+const userService = { logIn, register, getUsers, register }
 export default userService
