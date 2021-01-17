becea04bc0b5938edbb7f082db8faa1533faa1e9
Kujanenj <44529387+Kujanenj@users.noreply.github.com>
2021-01-17 17:34:18 +0200

    Add view for all users

---
+++ frontend/src/view/Users/AllUsers/AllUsers.js
@@ -0,0 +1,99 @@
+import React, { useEffect, useState } from 'react'
+import PerfectScrollbar from 'react-perfect-scrollbar'
+import { Typography } from '@material-ui/core'
+import { useDispatch } from 'react-redux'
+import { makeStyles } from '@material-ui/core/styles'
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow,
+  TablePagination
+} from '@material-ui/core'
+
+import { getUsers } from '../../../reducers/usersReducer'
+import { setAlert } from '../../../reducers/alertReducer'
+const useStyles = makeStyles((theme) => ({
+  table: {
+    minWidth: 750,
+    align: 'left'
+  }
+}))
+
+const AllUsers = ({ users }) => {
+  const classes = useStyles()
+  const dispatch = useDispatch()
+  const [page, setPage] = useState(0)
+  const [rowsPerPage, setRowsPerPage] = useState(10)
+  useEffect(() => {
+    dispatch(
+      getUsers()
+    ).catch((error) => {
+      dispatch(setAlert('Error', String(error), 5000))
+    })
+  }, [page, rowsPerPage, dispatch])
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage)
+  }
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10))
+    setPage(0)
+  }
+  // Workaround to making a row a link.
+  // You cant add <a> to a <tr> in order to make the whole row a link
+  // Believe me, i've tried
+  const handleRowClick = (event, user) => {
+    event.preventDefault()
+  }
+
+  if (!users) {
+    return <div></div>
+  }
+  return (
+    <React.Fragment>
+      <PerfectScrollbar>
+        <Table className={classes.table}>
+          <TableHead>
+            <TableRow>
+              <TableCell>Username</TableCell>
+
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {users.map((user) => (
+              <TableRow
+                hover={true}
+                key={user.id}
+                onClick={(event) => handleRowClick(event, user)}
+              >
+                <TableCell width="30%">
+                  <Typography>{user.username}</Typography>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+        <TablePagination
+          rowsPerPageOptions={[5, 10, 25]}
+          component="div"
+          count={-1}
+          //TODO:Replace this
+          rowsPerPage={rowsPerPage}
+          page={page}
+          label={{ page }}
+          onChangePage={handleChangePage}
+          onChangeRowsPerPage={handleChangeRowsPerPage}
+          labelDisplayedRows={({from,to,count})=>{
+            return `${from}-${to}`
+          }}
+          nextIconButtonProps={{ disabled: users.length < rowsPerPage }}
+        />
+      </PerfectScrollbar>
+    </React.Fragment>
+  )
+}
+
+export default AllUsers
---
+++ frontend/src/view/Users/AllUsers/index.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import { useSelector } from 'react-redux'
+import { Container, Grid, makeStyles } from '@material-ui/core'
+import Page from '../../../components/Page'
+import AllUsers from './AllUsers'
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    backgroundColor: theme.palette.background.dark,
+    minHeight: '100%',
+    paddingBottom: theme.spacing(3),
+    paddingTop: theme.spacing(3)
+  }
+}))
+
+const AllUsersView = () => {
+  const classes = useStyles()
+  // Workaround to making a row a link.
+  // You cant add <a> to a <tr> in order to make the whole row a link
+  // Believe me, i've tried
+
+  const users = useSelector(({ users }) => {
+    return users.allUsers
+  })
+  return (
+    <Page className={classes.root} title="AllUsers">
+      <Container maxWidth={false}>
+        <Grid container spacing={1}>
+          <Grid item sm={3}>
+            <AllUsers users={users}></AllUsers>
+          </Grid>
+          <Grid item sm={4}></Grid>
+        </Grid>
+      </Container>
+    </Page>
+  )
+}
+
+export default AllUsersView
